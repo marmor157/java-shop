52c6b41d6438429bfa883c427b5832949ed14a8e
Mariusz Morawski <mariusz.morawski@swmansion.com>
2021-01-15 21:40:31 +0100

    Added removing products from wishlist

---
+++ src/main/java/com/javashop/javashop/graphql/WishlistDataFetchers.java
@@ -0,0 +1,38 @@
+package com.javashop.javashop.graphql;
+
+import com.javashop.javashop.model.*;
+import com.javashop.javashop.repository.UserRepository;
+import com.javashop.javashop.repository.WarehouseRepository;
+import graphql.schema.DataFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+
+@Component
+public class WishlistDataFetchers {
+    @Autowired
+    private UserRepository userRepository;
+
+    public DataFetcher getUserWishlist() {
+        return  dataFetchingEnvironment -> {
+            Integer id = Integer.parseInt(dataFetchingEnvironment.getArgument("id"));
+            return  userRepository.getOne(id).getWishlist();
+        };
+    }
+
+    public DataFetcher deleteProductFromWishlist() {
+        return  dataFetchingEnvironment -> {
+            Integer user_id = Integer.parseInt(dataFetchingEnvironment.getArgument("user_id"));
+            Integer product_id = Integer.parseInt(dataFetchingEnvironment.getArgument("product_id"));
+            User user = userRepository.getOne(user_id);
+            user.getWishlist().removeIf(product->
+                    product.getId().compareTo(product_id) == 0
+            );
+            return userRepository.save(user);
+        };
+    }
+}
