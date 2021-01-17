135c984fcbcde3343f110be7544e4707640d9aa2
SPiotr568 <piotrsularz99@gmail.com>
2021-01-17 11:38:45 +0100

    Add filter complaintType

--- src/main/java/com/javashop/javashop/repository/ComplaintTypeRepository.java
+++ src/main/java/com/javashop/javashop/repository/ComplaintTypeRepository.java
@@ -1,10 +1,15 @@
 package com.javashop.javashop.repository;
 
 import com.javashop.javashop.model.ComplaintType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ComplaintTypeRepository extends JpaRepository<ComplaintType, Integer> {
 
+    List<ComplaintType> findByIdIn(List<Integer> id, Pageable pageable);
+    Long countByIdIn(List<Integer> id);
 }
